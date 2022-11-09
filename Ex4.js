40ff0b3cf49c5aa4234b22836837e7eed0af7031
AnzhelaDavityan <117297150+AnzhelaDavityan@users.noreply.github.com>
2022-11-09 23:14:50 +0400

    Add files via upload

---
+++ Ex4.js
@@ -0,0 +1,12 @@
+// 4.Using a for loop, write a program that reverses a given array.
+let array=[1,2,3];
+console.log("arr=",array,";")
+let result=[]
+for(let i=array.length-1;i>=0; i--){
+    result.push(array[i])
+}
+console.log("result=>",result,";")
+
+
+
+
